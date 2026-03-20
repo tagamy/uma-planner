577013d0a8251f24b1ad31c9ce935e7c43c7893f
Yosui Tagami <tagamy@gmail.com>
2026-03-20 12:17:14 +0900

    feat: setup GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.GITHUB_REPOSITORY ? `/${process.env.GITHUB_REPOSITORY.split('/')[1]}/` : '/',
   plugins: [
     react(),
     VitePWA({
